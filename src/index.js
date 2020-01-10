303339df5e8eebb851626cc99eb91cc0032dc16c
Tommy Schaefer <tommy.schaefer@teecom.com>
2020-01-10 08:55:25 -0600

    Move body styles to html element
    This change moves the body styles onto the html element to avoid styling
    issues when the page is hot reloaded.
    
    Fixes #12

--- src/index.js
+++ src/index.js
@@ -5,10 +5,3 @@ import { Elm } from './elm/Main.elm';
 Elm.Main.init({
   node: document.getElementById('root')
 });
-
-document.body.classList.add(
-  "bg-gray-900",
-  "antialiased",
-  "font-sans",
-  "text-white"
-);
