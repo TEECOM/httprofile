cb641fa6c2a88c1589996f837becf5ab145e8223
Tommy Schaefer <tommy.schaefer@teecom.com>
2020-01-12 21:03:48 -0600

    Add keyboard icon

--- src/elm/Icon.elm
+++ src/elm/Icon.elm
@@ -1,4 +1,4 @@
-module Icon exposing (bolt, downCarrot, heart, logo, minus, plus)
+module Icon exposing (bolt, downCarrot, heart, keyboard, logo, minus, plus)
 
 import Html exposing (Html)
 import Svg exposing (circle, path, rect, svg)
@@ -49,3 +49,10 @@ bolt =
     svg
         [ viewBox "0 0 20 20", class "fill-current h-4 hover:text-yellow-500" ]
         [ path [ d "M13 8V0L8.11 5.87 3 12h4v8L17 8h-4z" ] [] ]
+
+
+keyboard : Html Never
+keyboard =
+    svg
+        [ viewBox "0 0 20 20", class "fill-current h-6" ]
+        [ path [ d "M0 6c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6zm2 0v2h2V6H2zm1 3v2h2V9H3zm-1 3v2h2v-2H2zm3 0v2h10v-2H5zm11 0v2h2v-2h-2zM6 9v2h2V9H6zm3 0v2h2V9H9zm3 0v2h2V9h-2zm3 0v2h2V9h-2zM5 6v2h2V6H5zm3 0v2h2V6H8zm3 0v2h2V6h-2zm3 0v2h4V6h-4z" ] [] ]
